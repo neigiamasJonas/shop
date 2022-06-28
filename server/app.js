1ddb7f26d612da31788b6978fa77495800fe98db
neigiamasJonas <p.ezerskis@gmail.com>
2022-06-28 15:16:48 +0300

    pradzia nepilna

---
+++ server/app.js
@@ -0,0 +1,44 @@
+
+const express = require("express");
+const app = express();
+const port = 3006;
+const cors = require("cors");
+
+app.use(cors());
+
+const mysql = require("mysql");
+
+app.use(
+
+    express.urlencoded({
+
+        extended: true,
+
+    })
+
+);
+
+app.use(express.json());
+
+
+
+
+const con = mysql.createConnection({
+
+    host: "localhost",
+
+    user: "root",
+
+    password: "",
+
+    database: "ISIRASHOM DATABASE",
+
+});
+
+
+
+app.listen(port, () => {
+
+    console.log(`Bebras klauso porto Nr ${port}`);
+
+});
